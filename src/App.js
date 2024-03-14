335732f05b43c4dcdf20562058f819fd664fe60f
Rebecca Axelrod <reaxelrod@gmail.com>
2024-03-14 00:32:12 -0400

    This is the code so  far for the react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+    console.log('Constructor');
+  }
+
+
+  componentDidMount(){
+    console.log('componentDidMount')
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => 
+      this.setState(
+        ()=>{
+          return {monsters:  users} 
+        },
+        () => {
+          console.log(this.state);
+        }
+      )
+    );
+  }
+
+  onSearchChange = (event) => {
+    console.log(event.target.value);
+    const searchField = event.target.value.toLowerCase();
+    this.setState(()=>{
+      return {searchField};
+    })
+  }
+
+  render(){
+    console.log('render');
+
+    const {monsters, searchField} = this.state;
+    const {onSearchChange} = this;
+
+    const  filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField)
+    });
+
+    return( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input 
+        className='search-box' 
+        type='search' 
+        placeholder='search monsters' 
+        onChange={onSearchChange}
+      />
+      {/* {filteredMonsters.map((monster)=>{
+          return (
+          <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
+          );
+        })
+      } */}
+      <CardList/>
     </div>
-  );
-}
+    )
+  };
+};
 
 export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+
+class CardList extends Component{
+
+    render(){
+        return(
+            <div>
+                Hello I'm the CardList component.
+            </div>)
+    }
+}
+
+export default CardList;
